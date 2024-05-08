c6b6a8d951955a426655fe89149c63cbbe19e9e1
Tomás Badenes <tomasbadenes@gmail.com>
2024-05-08 19:04:46 -0300

    start.sh script and systemd service

---
+++ instock_connector/scripts/install_service.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# Installs a systemd service for the instock-connector
+
+
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 [--uninstall] <config_basename>.<robot_id>"
+    echo "It will create and enable /etc/systemd/system/instock-connector@<config_basename>.<robot_id>.service"
+    echo "The service will run the InOrbit Instock Connector the <config_basename>.yaml configuration and select the <robot_id> robot"
+    echo "If --uninstall is passed it will stop and remove the service"
+    exit 1
+fi
+
+echo This script will run commands with sudo. You may be prompted for your password
+read -n 1 -r -p "Do you wish to continue? [Y/n]" ans;
+case "$ans" in
+    n|N)
+        exit 1;;
+    Y|y|"")
+        ;;
+    *)
+        exit 1;;
+esac
+
+NAME=$1
+TEMPLATE="$( realpath $( dirname $0 ) )/instock-connector@.service"
+SYSTEMD_SERVICE_NAME="instock-connector@$1.service"
+START_SCRIPT_LOCATION="$( realpath $( dirname $0 ) )/start.sh"
+DESTINATION_START_SCRIPT="/usr/local/bin/instock-connector.sh"
+
+# Uninstall procedure
+if [ "$1" == "--uninstall" ]; then
+    if [ $# -ne 2 ]; then
+        echo "Usage: $0 --uninstall <service_name>"
+        echo Missing service name
+        exit 1
+    fi
+
+    echo Uninstalling systemd service instock-connector@$2.service
+    sudo systemctl stop instock-connector@$2.service
+    sudo systemctl disable instock-connector@$2.service
+    sudo rm /etc/systemd/system/instock-connector@$2.service
+    sudo systemctl daemon-reload
+
+    echo Removing $DESTINATION_START_SCRIPT
+    sudo rm $DESTINATION_START_SCRIPT
+    exit 0
+fi
+
+# Check if the inorbit user exists
+if ! id inorbit >/dev/null 2>&1; then
+    echo \`inorbit\` user not found. Creating it...
+    sudo useradd inorbit
+fi
+
+echo "Creating systemd service $SYSTEMD_SERVICE_NAME"
+sudo systemctl stop $SYSTEMD_SERVICE_NAME
+sudo cp $TEMPLATE /etc/systemd/system/$SYSTEMD_SERVICE_NAME
+sudo systemctl daemon-reload
+
+echo Installing symlink to $START_SCRIPT_LOCATION in $DESTINATION_START_SCRIPT
+sudo ln -sf $START_SCRIPT_LOCATION $DESTINATION_START_SCRIPT
+
+echo ""
+echo To start the service run:
+echo "	sudo systemctl start $SYSTEMD_SERVICE_NAME"
+echo To stop the service run:
+echo "	sudo systemctl stop $SYSTEMD_SERVICE_NAME"
+echo To enable the service at boot run:
+echo "	sudo systemctl enable $SYSTEMD_SERVICE_NAME"
+echo Watch logs with:
+echo "	sudo journalctl -u $SYSTEMD_SERVICE_NAME -f"

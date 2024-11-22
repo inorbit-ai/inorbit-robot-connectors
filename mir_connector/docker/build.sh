e20cef8a7989d1bba3aa2a144db7f53a9442eb3d
Tomás Badenes <tomasbadenes@gmail.com>
2024-11-22 10:36:06 -0300

    Suggestions

--- mir_connector/docker/build.sh
+++ mir_connector/docker/build.sh
@@ -2,27 +2,25 @@
 
 # This script builds the Docker image for the MIR Connector.
 # The image is tagged with the current version of the connector.
+
 # (maintainers only) the image is pushed to the Google Cloud Registry if the --push flag is provided.
+# Remember to first run `gcloud auth configure-docker us-central1-docker.pkg.dev` to authenticate
+# with the Google Cloud Registry.
+
+# Exit on error
+set -e
 
 CONNECTOR_VERSION=$(grep -oP '(?<=current_version = ")[^"]*' ../.bumpversion.toml)
+
 IMAGE_NAME="us-central1-docker.pkg.dev/inorbit-integrations/connectors/mir_connector:${CONNECTOR_VERSION}"
+IMAGE_NAME_SHORT="mir_connector:${CONNECTOR_VERSION}"
 
 echo "Building Docker image '$IMAGE_NAME'..."
 
 cd "$(dirname "$0")/../"
 
-docker build -t $IMAGE_NAME -f docker/Dockerfile .
-
-if [ $? -eq 0 ]; then
-    echo "Docker image '$IMAGE_NAME' built successfully."
-else
-    echo "Failed to build Docker image '$IMAGE_NAME'."
-    exit 1
-fi
+docker build -t $IMAGE_NAME -t $IMAGE_NAME_SHORT -f docker/Dockerfile .
 
 if [ "$1" == "--push" ]; then
     docker push $IMAGE_NAME
-    if [ $? -eq 0 ]; then
-        echo "Docker image '$IMAGE_NAME' pushed successfully."
-    fi
 fi

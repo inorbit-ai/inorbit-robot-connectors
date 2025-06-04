6234b7afbbc3c53d7d2d88126c05fd8569fe3c5a
Tomás Badenes <tomasbadenes@gmail.com>
2025-06-04 18:49:12 -0300

    Gausium Connector: Bump version: 0.1.0 → 0.2.0

--- gausium_connector/inorbit_gausium_connector/__init__.py
+++ gausium_connector/inorbit_gausium_connector/__init__.py
@@ -8,4 +8,4 @@ __author__ = """InOrbit Inc."""
 __email__ = "support@inorbit.ai"
 # Do not edit this string manually, always use bump-my-version.
 # See section "version-bump" in README.md
-__version__ = "0.1.0"
+__version__ = "0.2.0"

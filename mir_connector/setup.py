b13b8b758d01b5890b04a50c310dc000ecc993fb
Tomás Badenes <tomasbadenes@gmail.com>
2025-09-02 14:44:39 -0300

    Update dependencies

--- mir_connector/setup.py
+++ mir_connector/setup.py
@@ -7,10 +7,10 @@ from setuptools import setup
 
 requirements = [
     "requests>=2.31,<3.0",
-    "httpx",  # >=0.27,<0.28",
-    "inorbit-edge[video]>=1.17",
+    "httpx~=0.28",
+    "inorbit-edge[video]~=1.22",
     "inorbit-connector~=1.2.1",
-    "inorbit_edge_executor @ file:///home/tomas/InOrbit/inorbit_edge_executor",
+    "inorbit-edge-executor~=3.2.0",
     "prometheus-client>=0.14.1",
     "pytz>=2022.7",
     # NOTE: both pyyaml and ruamel.yaml packages are included here. Otherwise, the
@@ -30,7 +30,7 @@ test_requirements = [
     "requests_mock==1.11",
     "deepdiff==6.7",
     "pytest-asyncio>=0.23",
-    "pytest-httpx",  # >=0.30",
+    "pytest-httpx~=0.35",
 ]
 
 dev_requirements = {

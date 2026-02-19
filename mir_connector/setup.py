29f967c83fb0237ec863cd75bdd0c5864d1385a7
Tomás Badenes <tomasbadenes@gmail.com>
2026-02-19 11:34:26 -0300

    Bump mir_connector version: 1.0.3 → 1.0.4

--- mir_connector/setup.py
+++ mir_connector/setup.py
@@ -78,5 +78,5 @@ setup(
     python_requires=">=3.10",
     # Do not edit this string manually, always use bump-my-version. See
     # https://github.com/inorbit-ai/inorbit-robot-connectors/tree/main/mir_connector#version-bump
-    version="1.0.3",
+    version="1.0.4",
 )
